3341d14b939e9adb1c74076627becf01da307995
Shuchang Zheng <wintonzheng0325@gmail.com>
2023-03-23 00:37:40 -0700

    brew install list

---
+++ brew_install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+brew install ninja libtool cmake pkg-config gettext curl
+brew install neovim
+brew install byobu
+brew install ctags
+brew install tldr
+brew install poetry the_silver_searcher
